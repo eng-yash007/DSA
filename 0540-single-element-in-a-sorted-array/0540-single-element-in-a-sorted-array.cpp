dbb38434cd094a23c0cb6f4510a86551ef72c4c4
eng-yash007 <yashgupta9171@gmail.com>
2026-08-20 19:36:33 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.88%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int xor1 = 0;
-        for(int i=0;i<nums.size();i++){
-            xor1 = xor1^nums[i];
+        // (even, odd) -> we are at left part -> eliminate left
+        // (odd, even) -> we are at right part -> eliminate right
+        // we wil initally trim down the workspace form low and hight too
+        // we will check for low and high manually at the beginning
+        // we will compare mid to its left and right and check if it is not ewual to anyone of them if yes then return mid otherwise update left or right accordingly
+        // [even,odd, even,odd, even, odd,even]
+        int n = nums.size();
+        if(n ==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int low = 1;
+        int high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) return nums[mid];
+            else{
+                if((mid%2==1 && nums[mid] == nums[mid-1]) || mid%2==0 && nums[mid] == nums[mid+1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
         }
-        return xor1;
+        return nums[0];
     }
 };
