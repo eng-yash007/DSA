969625dff68eb447d749f88ebfa06288ed914fd0
eng-yash007 <yashgupta9171@gmail.com>
2026-09-04 00:12:35 +0530

    Time: 117 ms (45.38%), Space: 9 MB (82.9%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -9,7 +9,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             int freq[26] = {0};
-            for (int j = i; j < n; j++) {
+            for (int j = i; j < n; j++) {// ham har ek sub string k eliye freq nikal rhe h then beauty bhi har ek substrng kr liyr niakenge 
                 freq[s[j] - 'a']++;
 
                 int mx = INT_MIN;
