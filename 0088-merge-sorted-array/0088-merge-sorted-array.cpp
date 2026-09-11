eb6bd7c203c018095d24d71a3172d49ffb9e0147
eng-yash007 <yashgupta9171@gmail.com>
2026-09-11 19:03:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (86.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
+    void swaping(vector<int>& arr1,int idx1, vector<int>& arr2, int idx2){
+        //swaping kar rhe h 
+        if(arr1[idx1] > arr2[idx2]){
+            swap(arr1[idx1], arr2[idx2]);
+        }
+    }
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1;
-        int j = 0;
-        while(i>=0 && j<n){
-            if(nums1[i]> nums2[j]){
-                swap(nums1[i], nums2[j]);
-                i--;
-                j++;
-            }
-            else{
-                break;
+        int len = m+n;
+        int gap = (len/2) + (len%2); // ceil value leni h
+        while(gap>0){
+            int left = 0;
+            int right = left + gap;
+            while(right<len){
+                if(left<m && right>=m){ // arr1 and arr2 elements
+                    swaping(nums1, left, nums2, right-m);
+                }
+                else if(left>=m){ // arr2 and arr2 elements
+                    swaping(nums2, left-m, nums2, right-m);
+                }
 
+                else{ // arr1 and arr1 elements
+                    swaping(nums1, left, nums1, right);
+                }
+                left++;
+                right++;
             }
-
+            if(gap == 1) break;
+            gap = (gap/2) + (gap%2);
         }
-        sort(nums1.begin(), nums1.begin()+m);
-        sort(nums2.begin(), nums2.begin()+n);
-        for(int k = 0; k < n; k++) {
-            nums1[m + k] = nums2[k];
-        }
+        for(int i = 0; i < n; i++){
+            nums1[m + i] = nums2[i];
+        }   
        
     }
 };
