b9663241e256b14f5eaded469f2458bdd50f25c4
eng-yash007 <yashgupta9171@gmail.com>
2026-09-13 01:53:25 +0530

    Time: 2 ms (31.64%), Space: 95.2 MB (60.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,33 +4,46 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         int n = n1+n2;
-        vector<int> arr;
         int i=0,j=0;
+        int ind1 = n/2;
+        int ind2 = n/2 - 1;
+        int cnt = 0;
+        int ind1ele = -1;
+        int ind2ele = -1;
+
         while(i<n1 && j<n2){
             if(nums1[i] < nums2[j]){
-                arr.push_back(nums1[i]);
+                if(cnt == ind1) ind1ele = nums1[i];
+                if(cnt == ind2) ind2ele = nums1[i];
                 i++;
+                cnt++;
             }
             else{
-                arr.push_back(nums2[j]);
+                if(cnt == ind1) ind1ele = nums2[j];
+                if(cnt == ind2) ind2ele = nums2[j];
                 j++;
+                cnt++;
             }
         }
         while(i<n1){
-            arr.push_back(nums1[i]);
+            if(cnt == ind1) ind1ele = nums1[i];
+            if(cnt == ind2) ind2ele = nums1[i];
             i++;
+            cnt++;
         }
         while(j<n2){
-            arr.push_back(nums2[j]);
+            if(cnt == ind1) ind1ele = nums2[j];
+            if(cnt == ind2) ind2ele = nums2[j];
             j++;
+            cnt++;
         }
 
 
         if(n % 2 != 0){
-            return arr[n/2];
+            return ind1ele;
         }
         else{
-            return (arr[n/2 - 1] + arr[n/2]) / 2.0;
+            return (ind1ele + ind2ele) / 2.0;
         }
     }
 };
